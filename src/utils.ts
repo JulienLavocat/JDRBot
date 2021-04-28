e3ef4fd36f3e956c780944287b0bd4fb52fbf8d8
Julien Lavocat <julien.lavocat@gmail.com>
2021-04-28 23:06:32 +0200

    Added random success message

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,29 @@
 export const randomRange = (min: number, max: number) => {
-  return min + Math.floor(Math.random() * (max - min + 1));
+	return min + Math.floor(Math.random() * (max - min + 1));
 };
+
+const SUCCESS_EMOJI = "<:de:695949484097667092>";
+const FAIL_EMOJI = "<:issou:696128460740886590>";
+
+const SUCCESS_GIFS = [
+	"https://tenor.com/view/jim-carrey-ace-ventura-thumbs-up-smile-smiling-gif-5194908",
+	"https://tenor.com/view/yes-baby-goal-funny-face-gif-13347383",
+];
 
 export const getRollDiceMessage = (random: number, value: number) => {
-  if (value === 100) {
-    if (random <= 5)
-      return `${random} Succès critique ! <:de:695949484097667092>\nhttps://tenor.com/view/jim-carrey-ace-ventura-thumbs-up-smile-smiling-gif-5194908`;
+	if (value === 100) {
+		if (random <= 5)
+			return `${random} Succès critique ! ${SUCCESS_EMOJI}\n${getSuccessGif()}`;
 
-    if (random >= 95)
-      return `${random} Échec critique ! <:issou:696128460740886590> `;
+		if (random >= 95) return `${random} Échec critique ! ${FAIL_EMOJI}`;
 
-    if (random >= 90 && random < 95)
-      return `${random} C'est pas un échec critique ! C'est pas un échec critique !`;
-  }
+		if (random >= 90 && random < 95)
+			return `${random} C'est pas un échec critique ! C'est pas un échec critique !`;
+	}
 
-  return random;
+	return random;
 };
+
+export function getSuccessGif() {
+	return SUCCESS_GIFS[Math.floor(Math.random() * SUCCESS_GIFS.length)];
+}
