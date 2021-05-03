04475e5fef59ee1fb993c3dff0649fa14d50b68c
Julien Lavocat <julien.lavocat@gmail.com>
2021-05-03 22:11:34 +0200

    Added more gifs

--- src/utils.ts
+++ src/utils.ts
@@ -14,16 +14,29 @@ const FAIL_GIF = [
 	"https://tenor.com/view/gandalf-old-man-naked-take-robe-off-funny-gif-17224126",
 ];
 
+const NEAR_SUCCESS_GIF = [
+	"https://tenor.com/view/so-close-nick-young-for-three-in-and-out-lol-gif-16815386",
+	"https://tenor.com/view/so-close-basketball-thomas-morris-gif-18086098",
+];
+
+const NEAR_FAIL_GIF = [
+	"https://tenor.com/view/sweating-nervous-paranoid-gif-4974019",
+];
+
 export const getRollDiceMessage = (random: number, value: number) => {
+	random = 10;
 	if (value === 100) {
 		if (random <= 5)
 			return `${random} Succès critique ! ${SUCCESS_EMOJI}\n${getSuccessGif()}`;
+
+		if (random <= 10 && random > 5)
+			return `${random}\n${getNearSuccessGif()}`;
 
 		if (random >= 95)
 			return `${random} Échec critique ! ${FAIL_EMOJI}\n${getFailGif()}`;
 
 		if (random >= 90 && random < 95)
-			return `${random} C'est pas un échec critique ! C'est pas un échec critique !`;
+			return `${random} C'est pas un échec critique ! C'est pas un échec critique !\n${getNearFailGif()}`;
 	}
 
 	return random;
@@ -35,4 +48,14 @@ export function getSuccessGif() {
 
 export function getFailGif() {
 	return FAIL_GIF[Math.floor(Math.random() * FAIL_GIF.length)];
+}
+
+export function getNearSuccessGif() {
+	return NEAR_SUCCESS_GIF[
+		Math.floor(Math.random() * NEAR_SUCCESS_GIF.length)
+	];
+}
+
+export function getNearFailGif() {
+	return NEAR_FAIL_GIF[Math.floor(Math.random() * NEAR_FAIL_GIF.length)];
 }
